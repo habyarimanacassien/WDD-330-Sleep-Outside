049c170ed1ccaf3f24bb34e4cb8bdb69bd0279a7
eduardopinta93 <eduardo.pintam@gmail.com>
2026-03-14 23:18:11 +0100

    Cart improvements: empty cart fix and cart total feature

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,28 +1,42 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  const cartItems = getLocalStorage("so-cart") || [];
+
+  const productList = document.querySelector(".product-list");
+  const cartFooter = document.querySelector(".cart-footer");
+  const cartTotalElement = document.querySelector(".cart-total");
+
+  if (cartItems.length > 0) {
+    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+    productList.innerHTML = htmlItems.join("");
+
+    
+    const total = cartItems.reduce(
+      (sum, item) => sum + item.FinalPrice,
+      0
+    );
+
+    
+    cartFooter.classList.remove("hide");
+    cartTotalElement.innerHTML = `Total: $${total.toFixed(2)}`;
+  } else {
+    productList.innerHTML = "<p>Your cart is empty</p>";
+  }
 }
 
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
-
-  return newItem;
+  return `<li class="cart-card divider">
+    <a href="#" class="cart-card__image">
+      <img src="${item.Image}" alt="${item.Name}" />
+    </a>
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+  </li>`;
 }
 
 renderCartContents();
