6a0e684d8abd3aafec2b0075fe487afb282092eb
eduardopinta93 <eduardo.pintam@gmail.com>
2026-03-14 20:16:18 +0100

    W02 Individual Activity: Dynamic Product List

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,35 @@
+import { renderListWithTemplate } from "./utils.mjs";
+
+export default class ProductList {
+  constructor(category, dataSource, listElement) {
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
+
+  async init() {
+    const list = await this.dataSource.getData();
+    this.renderList(list);
+  }
+
+  renderList(products) {
+    renderListWithTemplate(
+      this.productCardTemplate,
+      this.listElement,
+      products,
+      "afterbegin",
+      true,
+    );
+  }
+
+  productCardTemplate(product) {
+  return `<li class="product-card">
+    <a href="product_pages/?product=${product.Id}">
+      <img src="${product.Image}" alt="${product.Name}">
+      <h3 class="card__brand">${product.Brand.Name}</h3>
+      <h2 class="card__name">${product.NameWithoutBrand}</h2>
+      <p class="product-card__price">$${product.FinalPrice}</p>
+    </a>
+  </li>`;
+}
+}
